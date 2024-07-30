8f37a03ab657b4b1206a0e9210abc4b2b114397d
Manny Sanchez <mannysanz715@gmail.com>
2024-07-30 16:04:08 -0700

    Create landing page folder, Add Title and RSVP Button

---
+++ src/app/landing/page.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+
+
+
+
+
+function Landing(){
+  return(
+    <main className="flex flex-col p-10 items-center h-screen">
+    <header className="flex flex-col items-center justify-between p-24 h-full">
+      <h1 className="text-4xl font-bold">Manuel & Mariam</h1>
+      <div className="hover:bg-sky-200 hover:shadow rounded-3xl bg-sky-300 text-white text-2xl font-bold text-center w-30 p-3">Rsvp Now</div>
+    </header>
+    </main>
+  )
+}
+
+export default Landing;
